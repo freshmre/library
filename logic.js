7b6c43cc4ff239df45f9b137f37e6da7aa7a7ca5
freshmre <adelrich@spoiledmre.com>
2021-08-30 02:18:09 +0100

    Add append books functionality

--- logic.js
+++ logic.js
@@ -17,5 +17,54 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(bookToAdd);
 }
 
+function makeBookDOMElement(book) {
+  let titleNode = document.createElement('div');
+  let authorNode = document.createElement('div');
+  let pagesNode = document.createElement('div');
+  let readNode = document.createElement('div');
+  let bookNode = document.createElement('div');
+
+  titleNode.classList.add('book-title');
+  titleNode.textContent = book.title;
+
+  authorNode.classList.add('book-author');
+  authorNode.textContent = book.author;
+
+  pagesNode.classList.add('pages');
+  pagesNode.textContent = book.pages;
+
+  readNode.classList.add('is-read');
+  if (book.read) {
+    readNode.classList.add('read');
+    readNode.textContent = 'finished';
+  }
+  else {
+    readNode.classList.add('unread');
+    readNode.textContent = 'unfinished';
+  }
+
+  bookNode.classList.add('book');
+  
+  bookNode.appendChild(titleNode);
+  bookNode.appendChild(authorNode);
+  bookNode.appendChild(pagesNode);
+  bookNode.appendChild(readNode);
+
+  return bookNode;
+}
+
+function updateEntriesList(entriesListDOM, bookList) {
+  bookList.forEach( book => { 
+    let newBook = makeBookDOMElement(book);
+    entriesListDOM.append(newBook);
+  })
+}
+
+let libraryEntries = document.getElementById('entries');
+
 addBookToLibrary('Industrial Society and Its Future', 'Theodore John Kaczynski', 188, true);
-console.log(myLibrary);
+addBookToLibrary('Industrial Society and Its Future', 'Theodore John Kaczynski', 188, true);
+addBookToLibrary('Industrial Society and Its Future', 'Theodore John Kaczynski', 188, true);
+addBookToLibrary('Industrial Society and Its Future', 'Theodore John Kaczynski', 188, true);
+
+updateEntriesList(libraryEntries, myLibrary);
